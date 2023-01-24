c7c1c3fb3d613f59d7ba27a4e2c425e3ba1e0561
Nathan Dean <nathandean@hotmail.co.uk>
2023-01-24 16:57:26 +0000

    Changed sm breakpoint

--- src/styling/theme.js
+++ src/styling/theme.js
@@ -57,7 +57,16 @@ const theme = responsiveFontSizes(createTheme({
                 }
             }
         }
-    }
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 700,
+            md: 900,
+            lg: 1200,
+            xl: 1536
+        },
+     }    
 }));
 
 const mobile = theme.breakpoints.down("sm");
